34ddbb25072ada06e8c0e5c68585183037bb17e8
RunesSI <rubenssi825@gmail.com>
2022-10-31 18:15:23 -0300

    corrigi bug da quarta questão

--- questao04/main.py
+++ questao04/main.py
@@ -5,26 +5,39 @@ def descriptografar(texto_cifrado, private_key):
     return rsa.decrypt(texto_cifrado, private_key)
 
 def carregarChaves():
-    with open("./keys/public.pem", "rb") as f:
-        public_key = rsa.PublicKey.load_pkcs1(f.read())
+    try: 
+        with open("./keys/public.pem", "rb") as f:
+            public_key = rsa.PublicKey.load_pkcs1(f.read())
 
-    with open("./keys/private.pem", "rb") as f:
-        private_key = rsa.PrivateKey.load_pkcs1(f.read())
+        with open("./keys/private.pem", "rb") as f:
+            private_key = rsa.PrivateKey.load_pkcs1(f.read())
+    except:
+        print("Não foi possivel carregar o arquivo!")
+
     return private_key, public_key
 
 def carregarAssinatura():
-    with open("./assinatura/assinatura", "rb") as f:
-        assinatura = f.read()
+    try:
+        with open("./assinatura/assinatura", "rb") as f:
+            assinatura = f.read()
+    except:
+        print("Não foi possível carregar o arquivo!")
     return assinatura
 
-private_key, public_key = carregarChaves()
-assinatura = carregarAssinatura()
+def main():
 
-mensagem = open("./mensagem/texto_assinado", "rb").read()
+    private_key, public_key = carregarChaves()
 
-if rsa.verify(mensagem, assinatura, public_key):
-    texto = descriptografar(mensagem, private_key)
-    print(texto)
+    assinatura = carregarAssinatura()
 
+    mensagem = open("./mensagem/texto_assinado", "rb").read()
 
+    if rsa.verify(mensagem, assinatura, public_key):
+        print("Assinatura Válida!")
+        texto = descriptografar(mensagem, private_key)
+        print(texto)
+    else:
+        print("Assinatura Válida!")
+
+main()
 
